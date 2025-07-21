6cc460a31a20a6479b72baa8f97fd6a0da684d2b
xeony <71081051+xeony888@users.noreply.github.com>
2025-07-20 22:47:41 -0400

    update

--- components/utils.ts
+++ components/utils.ts
@@ -2,9 +2,11 @@ import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import BN from "bn.js";
 
 export function calculateReward(bids: BN, id: BN, amount: BN): BN {
-    const sum = bids.mul(bids.add(new BN(1))).div(new BN(2));
-    const distance = bids.sub(id);
-    return distance.mul(amount).div(sum);
+    if (bids.eq(id)) {
+        return amount.div(new BN(2))
+    } else {
+        return amount.div(new BN(2)).div(bids.sub(new BN(1)))
+    }
 }
 export function calculateBidCost(fee: BN, num: BN): number {
     return fee.mul(num.pow(new BN(2))).toNumber() / LAMPORTS_PER_SOL;
